12be26ebf9c37e876074f5ab796cc7e14c064f59
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-06-21 08:16:08 +0530

    Add files via upload

---
+++ Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>   //syntax
+#include<conio.h>
+int main ()
+{
+    int numArray[10], i;    //input in array for using loop
+    for(i=0; i<10; i++)
+    {
+        printf("%d\n",i+1);     //display
+       
+    }
+    return 0;
+}
