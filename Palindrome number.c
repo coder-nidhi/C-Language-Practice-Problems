98229c7145de450be2db42e6782b685a41572af7
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-02 20:24:16 +0530

    Add files via upload

---
+++ Palindrome number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, reverse=0, remainder=0, original;
+    
+    printf("Enter a positive number\n");
+    scanf("%d", &n);
+    
+    original=n;
+    
+    while(n!=0)
+    {
+        remainder = n % 10;
+        reverse = reverse* 10 + remainder;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
+
