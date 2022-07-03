9b28e788dc543eb7452bf240971a2c270a230204
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-03 09:14:07 +0530

    Add files via upload

---
+++ Division.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,ans;
+    printf("Enter a number a : ");
+    scanf("%f", &a);
+    printf("Enter a number b : ");
+    scanf("%f" , &b);
+   
+    
+    ans=a/b;
+    printf("Division of 2 numbers is %.2f",ans);
+    return 0;
+}
+
+
