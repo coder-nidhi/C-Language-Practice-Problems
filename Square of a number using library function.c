f4f16e38025b1066060f61f216c6d11fecdf11fb
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-28 20:29:03 +0530

    Add files via upload

---
+++ Square of a number using library function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    float n;
+    
+    printf("Enter a number to find it's square : ");
+    scanf("%f",&n);
+    printf("%f", pow(n,2));
+    
+
+    return 0;
+}
+
