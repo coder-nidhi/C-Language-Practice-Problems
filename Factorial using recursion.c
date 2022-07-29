d6c04707ad77ecc76283eec2a5a523b54e8e13f9
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-07-29 23:06:28 +0530

    Add files via upload

---
+++ Factorial using recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fact(int n);
+
+int main()
+{
+    int n;
+    
+    printf("Enter a number to find factorial :  ");
+    scanf("%d",&n);
+    printf("factorial is : %d ",fact(n));
+
+    return 0;
+}
+
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    int factNm1 = fact(n-1);
+    int factN = factNm1 * n;
+    return factN;
+}
