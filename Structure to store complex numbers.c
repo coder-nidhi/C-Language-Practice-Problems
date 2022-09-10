e957604ae35b3b8c977ac7150762638c10c6ab53
NIDHI MAYUR THAKKAR <107824852+coder-nidhi@users.noreply.github.com>
2022-09-10 21:02:01 +0530

    Add files via upload

---
+++ Structure to store complex numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct complex
+{
+    int real;
+    int img;
+    
+};
+
+int main()
+{
+    struct complex number1 = {5,8};
+    struct complex *ptr = &number1;
+    
+    printf("Real part = %d\n",ptr->real);
+    printf("Imaginary part = %d\n",ptr->img);
+
+    return 0;
+}
+
